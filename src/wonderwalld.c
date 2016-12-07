f7b11354af70452f6333a8f0a174321e0d521ea0
Hugo Freire <hfreire@exec.sh>
2016-12-08 00:05:57 +0100

    Added rabbitmq library calls.

--- src/wonderwalld.c
+++ src/wonderwalld.c
@@ -5,33 +5,242 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-void print_usage() {
-    printf("Usage: wonderwallc <-b broker hostname>\n");
+#include <amqp_tcp_socket.h>
+
+#include <sys/time.h>
+
+#include <stdarg.h>
+
+#define SUMMARY_EVERY_US 1000000
+
+uint64_t now_microseconds(void) {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (uint64_t) tv.tv_sec * 1000000 + (uint64_t) tv.tv_usec;
 }
 
-int main(int argc, char **argv) {
-    char *broker = NULL;
-    int i;
+void microsleep(int usec) {
+    struct timespec req;
+    req.tv_sec = 0;
+    req.tv_nsec = 1000 * usec;
+    nanosleep(&req, NULL);
+}
 
-    for (i = 1; i < argc; i++) {
-        if (argv[i] == NULL || argv[i][0] != '-') break;
-        switch (argv[i][1]) {
-            case 'h':
-                broker = argv[++i];
-                break;
-            default:
-                fprintf(stderr, "%s: %c: unknown option\n", argv[0], argv[i][1]);
-                print_usage();
+void die(const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
 
-                exit(0);
+void die_on_error(int x, char const *context) {
+    if (x < 0) {
+        fprintf(stderr, "%s: %s\n", context, amqp_error_string2(x));
+        exit(1);
+    }
+}
+
+void die_on_amqp_error(amqp_rpc_reply_t x, char const *context) {
+    switch (x.reply_type) {
+        case AMQP_RESPONSE_NORMAL:
+            return;
+
+        case AMQP_RESPONSE_NONE:
+            fprintf(stderr, "%s: missing RPC reply type!\n", context);
+            break;
+
+        case AMQP_RESPONSE_LIBRARY_EXCEPTION:
+            fprintf(stderr, "%s: %s\n", context, amqp_error_string2(x.library_error));
+            break;
+
+        case AMQP_RESPONSE_SERVER_EXCEPTION:
+            switch (x.reply.id) {
+                case AMQP_CONNECTION_CLOSE_METHOD: {
+                    amqp_connection_close_t *m = (amqp_connection_close_t *) x.reply.decoded;
+                    fprintf(stderr, "%s: server connection error %uh, message: %.*s\n",
+                            context,
+                            m->reply_code,
+                            (int) m->reply_text.len, (char *) m->reply_text.bytes);
+                    break;
+                }
+                case AMQP_CHANNEL_CLOSE_METHOD: {
+                    amqp_channel_close_t *m = (amqp_channel_close_t *) x.reply.decoded;
+                    fprintf(stderr, "%s: server channel error %uh, message: %.*s\n",
+                            context,
+                            m->reply_code,
+                            (int) m->reply_text.len, (char *) m->reply_text.bytes);
+                    break;
+                }
+                default:
+                    fprintf(stderr, "%s: unknown server error, method id 0x%08X\n", context, x.reply.id);
+                    break;
+            }
+            break;
+    }
+
+    exit(1);
+}
+
+
+static void run(amqp_connection_state_t conn) {
+    uint64_t start_time = now_microseconds();
+    int received = 0;
+    int previous_received = 0;
+    uint64_t previous_report_time = start_time;
+    uint64_t next_summary_time = start_time + SUMMARY_EVERY_US;
+
+    amqp_frame_t frame;
+
+    uint64_t now;
+
+    for (;;) {
+        amqp_rpc_reply_t ret;
+        amqp_envelope_t envelope;
+
+        now = now_microseconds();
+        if (now > next_summary_time) {
+            int countOverInterval = received - previous_received;
+            double intervalRate = countOverInterval / ((now - previous_report_time) / 1000000.0);
+            printf("%d ms: Received %d - %d since last report (%d Hz)\n",
+                   (int) (now - start_time) / 1000, received, countOverInterval, (int) intervalRate);
+
+            previous_received = received;
+            previous_report_time = now;
+            next_summary_time += SUMMARY_EVERY_US;
+        }
+
+        amqp_maybe_release_buffers(conn);
+        ret = amqp_consume_message(conn, &envelope, NULL, 0);
+
+        if (AMQP_RESPONSE_NORMAL != ret.reply_type) {
+            if (AMQP_RESPONSE_LIBRARY_EXCEPTION == ret.reply_type &&
+                AMQP_STATUS_UNEXPECTED_STATE == ret.library_error) {
+                if (AMQP_STATUS_OK != amqp_simple_wait_frame(conn, &frame)) {
+                    return;
+                }
+
+                if (AMQP_FRAME_METHOD == frame.frame_type) {
+                    switch (frame.payload.method.id) {
+                        case AMQP_BASIC_ACK_METHOD:
+                            /* if we've turned publisher confirms on, and we've published a message
+                             * here is a message being confirmed
+                             */
+
+                            break;
+                        case AMQP_BASIC_RETURN_METHOD:
+                            /* if a published message couldn't be routed and the mandatory flag was set
+                             * this is what would be returned. The message then needs to be read.
+                             */
+                        {
+                            amqp_message_t message;
+                            ret = amqp_read_message(conn, frame.channel, &message, 0);
+                            if (AMQP_RESPONSE_NORMAL != ret.reply_type) {
+                                return;
+                            }
+
+                            amqp_destroy_message(&message);
+                        }
+
+                            break;
+
+                        case AMQP_CHANNEL_CLOSE_METHOD:
+                            /* a channel.close method happens when a channel exception occurs, this
+                             * can happen by publishing to an exchange that doesn't exist for example
+                             *
+                             * In this case you would need to open another channel redeclare any queues
+                             * that were declared auto-delete, and restart any consumers that were attached
+                             * to the previous channel
+                             */
+                            return;
+
+                        case AMQP_CONNECTION_CLOSE_METHOD:
+                            /* a connection.close method happens when a connection exception occurs,
+                             * this can happen by trying to use a channel that isn't open for example.
+                             *
+                             * In this case the whole connection must be restarted.
+                             */
+                            return;
+
+                        default:
+                            fprintf(stderr, "An unexpected method was received %u\n", frame.payload.method.id);
+                            return;
+                    }
+                }
+            }
+
+        } else {
+            amqp_destroy_envelope(&envelope);
+        }
+
+        received++;
+    }
+}
+
+int main(int argc, char const *const *argv) {
+    char const *hostname;
+    int port, status;
+    char const *exchange;
+    char const *bindingkey;
+    amqp_socket_t *socket = NULL;
+    amqp_connection_state_t conn;
+
+    amqp_bytes_t queuename;
+
+    if (argc < 3) {
+        fprintf(stderr, "Usage: amqp_consumer host port\n");
+        return 1;
+    }
+
+    hostname = argv[1];
+    port = atoi(argv[2]);
+    exchange = "amq.direct"; /* argv[3]; */
+    bindingkey = "test queue"; /* argv[4]; */
+
+    conn = amqp_new_connection();
+
+    socket = amqp_tcp_socket_new(conn);
+    if (!socket) {
+        die("creating TCP socket");
+    }
+
+    status = amqp_socket_open(socket, hostname, port);
+    if (status) {
+        die("opening TCP socket");
+    }
+
+    die_on_amqp_error(amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "rabbitmq", "rabbitmq"),
+                      "Logging in");
+    amqp_channel_open(conn, 1);
+    die_on_amqp_error(amqp_get_rpc_reply(conn), "Opening channel");
+
+    {
+        amqp_queue_declare_ok_t *r = amqp_queue_declare(conn, 1, amqp_empty_bytes, 0, 0, 0, 1,
+                                                        amqp_empty_table);
+        die_on_amqp_error(amqp_get_rpc_reply(conn), "Declaring queue");
+        queuename = amqp_bytes_malloc_dup(r->queue);
+        if (queuename.bytes == NULL) {
+            fprintf(stderr, "Out of memory while copying queue name");
+            return 1;
         }
     }
 
-    if (broker == NULL) {
-        print_usage();
-        exit(0);
-    }
+    amqp_queue_bind(conn, 1, queuename, amqp_cstring_bytes(exchange), amqp_cstring_bytes(bindingkey),
+                    amqp_empty_table);
+    die_on_amqp_error(amqp_get_rpc_reply(conn), "Binding queue");
+
+    amqp_basic_consume(conn, 1, queuename, amqp_empty_bytes, 0, 1, 0, amqp_empty_table);
+    die_on_amqp_error(amqp_get_rpc_reply(conn), "Consuming");
+
+    run(conn);
+
+    die_on_amqp_error(amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS), "Closing channel");
+    die_on_amqp_error(amqp_connection_close(conn, AMQP_REPLY_SUCCESS), "Closing connection");
+    die_on_error(amqp_destroy_connection(conn), "Ending connection");
+
+    return 0;
 }
