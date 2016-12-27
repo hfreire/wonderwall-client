dcae163a4eb23e8a7b21d80f3282edb2ac6b9c89
Hugo Freire <hfreire@exec.sh>
2016-12-27 18:55:15 +0100

    Improvements to toolchain.

--- cmake/platforms/rpi/toolchain/armv6-rpi1-linux-gnueabihf.cmake
+++ cmake/platforms/rpi/toolchain/armv6-rpi1-linux-gnueabihf.cmake
@@ -2,6 +2,6 @@ set(CMAKE_C_COMPILER armv6-rpi1-linux-gnueabihf-gcc)
 set(CMAKE_CXX_COMPILER armv6-rpi1-linux-gnueabihf-g++)
 set(CMAKE_ASM_COMPILER armv6-rpi1-linux-gnueabihf-gcc)
 
-add_definitions("-mcpu=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard -marm")
+add_definitions("-mcpu=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard -mlittle-endian -munaligned-access -marm")
 
 include("${CMAKE_CURRENT_LIST_DIR}/arm-linux-gnueabihf.cmake")
