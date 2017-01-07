6963c7e8866f0827e517ba3676b8e989d5a65ef6
Hugo Freire <hfreire@exec.sh>
2017-01-07 12:30:00 +0100

    Extracted toolchain files into its own repo.

--- cmake/platforms/rpi/toolchain/armv7-rpi2-linux-gnueabihf.cmake
+++
@@ -1,7 +0,0 @@
-set(CMAKE_C_COMPILER armv7-rpi2-linux-gnueabihf-gcc)
-set(CMAKE_CXX_COMPILER armv7-rpi2-linux-gnueabihf-g++)
-set(CMAKE_ASM_COMPILER armv7-rpi2-linux-gnueabihf-gcc)
-
-add_definitions("-mcpu=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard -mlittle-endian -munaligned-access -marm")
-
-include("${CMAKE_CURRENT_LIST_DIR}/arm-linux-gnueabihf.cmake")
